bf32e639851a828923b9afdbecb335f5963ac81a
DragonShadow343 <156151140+DragonShadow343@users.noreply.github.com>
2024-01-23 08:54:22 -0800

    Added my productive example of webscrapper
    The uniscrapper scrapped through possible choices for courses

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=software&txtLocation=').text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+b = 0
 
-for job in jobs:
-    company_name = job.find('h3', class_='joblist-comp-name').text.strip()
-    skills = job.find('span', class_='srp-skills').text.strip()
-    print(company_name)
-    print(skills)
-    print(" ")
+for i in range(0,200):
+    University_site = requests.get(f'https://www.utoronto.ca/news/searchnews?query_news=&field_topic=2812&changed%5Bdate%5D=&changed_1%5Bdate%5D=&page={i-1}').text
+    soup = BeautifulSoup(University_site, 'lxml')
+    articles = soup.find_all('h4', class_='field-content')
+    for a in articles:
+        link = a.a['href']
+        a = a.text
+        date = soup.find('p', class_='field-content').text
+
+        if ('software' in a) or ('engineering' in a) or ('robots' in a) or ('physic' in a) or ('green' in a) or ('AI' in a):
+
+            print(f"Page {i}")
+            print(f"Date: {date}")
+            b = b + 1
+            print(f"{b}. {a}")
+            print(f"Link: https://www.utoronto.ca{link}")
+            print("\n")
